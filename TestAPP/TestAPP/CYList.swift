4a1f0d60a5ddc7cefc84ce0b823549e0796f4a27
louchu0604 <louchu0604@gmail.com>
2017-09-08 01:48:08 +0800

    linked list 未测试

--- TestAPP/TestAPP/CYList.swift
+++ TestAPP/TestAPP/CYList.swift
@@ -21,11 +21,12 @@ class LinkedList {
     var size = 0
     //指定位置插入
     func insertToindex(_ index:Int,val:Int)-> Bool {
-        if (index>size) {
-            return false
-        }
         if (head==nil) {
-            
+            head = ListNode.init(val);
+            head?.next = nil;
+            size += 1
+        }
+        if (index>size) {
             return false
         }
         var  tempNode = head
@@ -46,14 +47,58 @@ class LinkedList {
     }
     //尾插法
     func appendToTail(_ val:Int) -> Bool {
-        return false
+        if (tail==nil) {
+            tail = ListNode.init(val);
+            tail?.next = nil;
+            size+=1
+        }else
+        {
+            let oldTail = tail;
+            let insertNode = ListNode.init(val);
+            insertNode.next = nil;
+            oldTail?.next = insertNode;
+            tail = insertNode;
+        }
+        size += 1
+
+        return true
     }
     //头插法
     func appendToHead(_ val:Int) -> Bool {
-        return false
+        if (head==nil) {
+            head = ListNode.init(val);
+            head?.next = nil;
+            size+=1
+        }else
+        {
+            let oldnhead = head
+            let insertNode = ListNode.init(val)
+            insertNode.next = oldnhead
+            head = insertNode
+        }
+        size += 1
+        
+        return true
     }
     
     func remove(_ index:Int) -> Bool {
+        if (index>size) {
+            return false
+        }
+        var  tempNode = head
+        
+        for i in 0..<index {
+            if ((tempNode?.next) == nil) {
+                return false
+            }
+            tempNode = tempNode?.next//取出要删的前一个
+        }
+        
+        let toremove = tempNode?.next//要删除的这一个
+        let nextofremove = toremove?.next//要删除的后一个
+        tempNode?.next = nextofremove;//
+        
+        size-=1;
         return false
     }
     func getSize() -> Int {
