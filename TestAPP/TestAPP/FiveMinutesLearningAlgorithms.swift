a75412dbf2838288d4e1e37f63165cbad2750d37
louchu <louchu0604@gmail.com>
2018-12-06 13:22:07 +0800

    update

--- TestAPP/TestAPP/FiveMinutesLearningAlgorithms.swift
+++ TestAPP/TestAPP/FiveMinutesLearningAlgorithms.swift
@@ -400,7 +400,14 @@ func twosumofarray(array:[Int],target:Int) -> [Int]! {
 
 
 //MARK: leetcode上第447号问题：Number of Boomerangs
-/*给定平面上 n 对不同的点，“回旋镖” 是由点表示的元组 (i, j, k) ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+/*
+ 原文描述：
+ Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that the distance between iand j equals the distance between i and k (the order of the tuple matters).
+ 
+ Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000](inclusive).
+ 
+ 
+ 给定平面上 n 对不同的点，“回旋镖” 是由点表示的元组 (i, j, k) ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
  找到所有回旋镖的数量。你可以假设 n 最大为 500，所有点的坐标在闭区间 [-10000, 10000] 中。
  示例:
  输入:
@@ -410,7 +417,24 @@ func twosumofarray(array:[Int],target:Int) -> [Int]! {
  解释:
  两个回旋镖为 [[1,0],[0,0],[2,0]] 和 [[1,0],[2,0],[0,0]] */
 
+//计算平方和
+//嵌套3层循环
 
+
+func numberOfBoomerangs(points:inout[[Int]]) -> [[Int]]? {
+    
+    for aim in 0..<points.count {
+        for first in 0..<points.count
+        {
+            for second in 0..<points.count
+            {
+                
+               
+            }
+        }
+    }
+    return [[]]
+}
 
 
 //MARK: leetcode上第219号问题：Contains Duplicate II
---
+++ TestAPP/TestAPP/MyUtils.swift
@@ -0,0 +1,17 @@
+//
+//  MyUtils.swift
+//  TestAPP
+//
+//  Created by louchu on 2018/12/6.
+//  Copyright © 2018年 Cy Lou. All rights reserved.
+//
+
+import Foundation
+class Point {
+    var x:Int
+    var y:Int
+    init(xValue:Int,yValue:Int) {
+        x = xValue
+        y = yValue
+    }
+}
