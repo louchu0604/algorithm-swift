8c004b4809a840011924c52edfb4a4e2c475aefd
louchu <louchu0604@gmail.com>
2019-06-21 14:37:17 +0800

    全排列

---
+++ TestAPP/TestAPP/NO.46permute.swift
@@ -0,0 +1,35 @@
+//
+//  NO.46permute.swift
+//  TestAPP
+//
+//  Created by louchu on 2019/6/21.
+//  Copyright © 2019年 Cy Lou. All rights reserved.
+//
+
+import Foundation
+/*
+ 给定一个没有重复数字的序列，返回其所有可能的全排列。
+ 
+ 输入: [1,2,3]
+ 输出:
+ [
+ [1,2,3],
+ [1,3,2],
+ [2,1,3],
+ [2,3,1],
+ [3,1,2],
+ [3,2,1]
+ ]
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/permutations
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ 
+ */
+func permute(_ nums: [Int]) -> [[Int]] {
+    return [[1,2,3]]
+}
+func getSub(_ nums: [Int],_ restIndex:[Int]) -> [[Int]]{
+    
+     return [[1,2,3]]
+}
